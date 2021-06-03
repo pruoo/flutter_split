851ba40e42e86ef0f0d84dfe3c7f576d88983c0e
justshreyas <professionally.shreyas@gmail.com>
2021-06-03 18:20:20 +0530

    Add MethodChannel call handling to SwiftPlugin

--- ios/Classes/SwiftFlutterSplitPlugin.swift
+++ ios/Classes/SwiftFlutterSplitPlugin.swift
@@ -17,21 +17,72 @@ public class SwiftFlutterSplitPlugin: NSObject, FlutterPlugin {
         switch (call.method) {
         case "initializeSdk":
             let args = call.arguments as! Dictionary<String, Any>
-            
             // Split API Key
             let apiKey: String = args["appKey"] as! String            
             // User id as Key
             let user: Key = Key(matchingKey: args["userId"] as! String)
             
-            splitDelegate.initializeSdk(apiKey:apiKey,user:user,result:result);
+            splitDelegate.initializeSdk(
+                apiKey: apiKey,
+                user: user,
+                result: result
+            );
+            
+            break;
+            
+        case "dispose":
+            // splitDelegate.dispose(result:result)
             break;
             
         case "getTreatment":
             let args = call.arguments as! Dictionary<String, Any>
             let splitName: String = args["key"] as! String
             let attributes: [String:Any] = args["attributes"] as! [String:Any]
-            splitDelegate.getTreatment(splitName:splitName,attributes:attributes,result:result);
             
+            splitDelegate.getTreatment(
+                splitName: splitName,
+                attributes: attributes,
+                result: result
+            );
+            
+            break;
+            
+            
+        case "getTreatments":
+            let args = call.arguments as! Dictionary<String, Any>
+            let splitNames: [String] = args["keys"] as! [String]
+            let attributes: [String:Any] = args["attributes"] as! [String:Any]
+            
+            // splitDelegate.getTreatments(splitNames: splitNames,attributes: attributes,result: result)
+            break; 
+            
+            
+        case "getTreatmentWithConfig":
+            
+            let args = call.arguments as! Dictionary<String, Any>
+            let splitName: String = args["key"] as! String
+            let attributes: [String:Any] = args["attributes"] as! [String:Any]
+            
+            // splitDelegate.getTreatmentWithConfig(splitName: splitName,attributes: attributes,result: result)
+            break;
+            
+        case "getTreatmentsWithConfig":
+            
+            let args = call.arguments as! Dictionary<String, Any>
+            let splitNames: [String] = args["keys"] as! [String]
+            let attributes: [String:Any] = args["attributes"] as! [String:Any]
+            
+            // splitDelegate.getTreatmentsWithConfig(splitNames: splitNames,attributes: attributes,result: result)
+            break;
+            
+            
+        case "trackEvent":
+            
+            let args = call.arguments as! Dictionary<String, Any>
+            let eventName: String = args["eventName"] as! String
+            let properties: [String:Any] = args["properties"] as! [String:Any]
+            
+            // splitDelegate.trackEvent(eventName: eventName,properties: properties,result: result)
             break;
             
         default:
