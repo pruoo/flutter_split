1a791ba0dd9664a437355cdb4bafd4f6d762a68c
justshreyas <professionally.shreyas@gmail.com>
2021-06-14 17:13:42 +0530

    Remove unused import

--- test/flutter_split_test.dart
+++ test/flutter_split_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_split/flutter_split.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_split');
