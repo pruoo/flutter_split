b548b28ea18f7252c689e11be2828657d3c3ee27
Jaya Sharma <jayasharma100197@gmail.com>
2021-10-07 16:44:04 +0530

    Split Bug Fix

--- android/src/main/java/com/imumz/flutter_split/FlutterSplitPlugin.java
+++ android/src/main/java/com/imumz/flutter_split/FlutterSplitPlugin.java
@@ -11,6 +11,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
+import android.util.Log;
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -23,8 +25,10 @@ import io.split.android.client.SplitFactory;
 import io.split.android.client.SplitFactoryBuilder;
 import io.split.android.client.SplitResult;
 import io.split.android.client.api.Key;
+import io.split.android.client.dtos.Split;
 import io.split.android.client.events.SplitEvent;
 import io.split.android.client.events.SplitEventTask;
+
 
 /** FlutterSplitPlugin */
 public class FlutterSplitPlugin implements FlutterPlugin, MethodCallHandler {
@@ -42,6 +46,8 @@ public class FlutterSplitPlugin implements FlutterPlugin, MethodCallHandler {
   // error codes
   private static String SDK_NOT_INITIALIZED = "SNI001";
 
+  private static boolean isSDKReady;
+
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
@@ -51,9 +57,13 @@ public class FlutterSplitPlugin implements FlutterPlugin, MethodCallHandler {
   }
 
   @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+  public void onMethodCall(@NonNull MethodCall call, final @NonNull Result result) {
     if(call.method.equals("initializeSdk")){
 
+
+      Log.i("split_client_init_1", "split_init");
+
+      isSDKReady = false;
       this.apikey = call.argument("appKey");
       SplitClientConfig config = SplitClientConfig.builder().build();
 
@@ -64,6 +74,7 @@ public class FlutterSplitPlugin implements FlutterPlugin, MethodCallHandler {
       SplitFactory splitFactory = null;
       try {
         splitFactory = SplitFactoryBuilder.build(this.apikey, k, config, this.appContext);
+        Log.i("split_client_init_2", "split_init_2");
       } catch (IOException e) {
         e.printStackTrace();
       } catch (InterruptedException e) {
@@ -73,10 +84,60 @@ public class FlutterSplitPlugin implements FlutterPlugin, MethodCallHandler {
       } catch (URISyntaxException e) {
         e.printStackTrace();
       }
+      
       // Get Split Client instance
+      Log.i("split_client_init_3", "split_init_3");      
       this.client = splitFactory.client();
 
-    }else if(call.method.equals("trackEvent")){
+      this.client.on( SplitEvent.SDK_READY,new SplitEventTask() {
+
+        @Override
+        public void onPostExecution(SplitClient client) {
+          super.onPostExecution(client);
+          isSDKReady = true;
+          Log.i("split_sdk_ready1", "split_sdk_ready1");
+          // result.success(true);
+        }
+
+        @Override
+        public void onPostExecutionView(SplitClient client) {
+          super.onPostExecutionView(client);
+          // isSDKReady = true;
+          
+          Log.i("split_sdk_ready2", "split_sdk_ready2");
+          System.out.println("split_sdk_ready2");
+          // result.success(true);
+        }
+      });
+
+        // When definitions were loaded from cache
+      this.client.on(SplitEvent.SDK_READY_FROM_CACHE, new SplitEventTask() {
+          @Override
+          public void onPostExecution(SplitClient client) {
+            //Background Code in Here
+            // super.onPostExecution(client);
+            isSDKReady = true;
+            Log.i("split_sdk_ready1", "split_sdk_ready_from_cache " + client.isReady());
+          }
+          @Override
+          public void onPostExecutionView(SplitClient client) {
+            //UI Code in Here
+          }
+        });
+        // When the SDK couldn't fetch definitions befor *config.ready* time
+      this.client.on(SplitEvent.SDK_READY_TIMED_OUT, new SplitEventTask() {
+          @Override
+          public void onPostExecution(SplitClient client) {
+            //Background Code in Here
+            Log.i("split_sdk_ready1", "split_sdk_ready_timed_out");
+          }
+          @Override
+          public void onPostExecutionView(SplitClient client) {
+            //UI Code in Here
+          }
+        });      
+
+    } else if(call.method.equals("trackEvent")){
       String eventType  = call.argument("eventName");
       HashMap<String,Object> map = new HashMap<String,Object>();
       if(this.client!=null){
